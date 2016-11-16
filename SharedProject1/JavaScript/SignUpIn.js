bffc72b3b0153c7bf1cb4f00c524880abe350958
oconnj01 <oconnj01@uwosh.edu>
2016-11-16 17:48:04 -0600

    Did register page. But doesn't use bootstrap. Will need to change login to match. Will do.

--- SharedProject1/JavaScript/SignUpIn.js
+++ SharedProject1/JavaScript/SignUpIn.js
@@ -1,22 +1,77 @@
 ﻿"use strict";
-window.onload = startup;
 
-function startUp()
+window.onload = startUp;
+
+function startUp() {
+
+    document.getElementById("user").addEventListener("keyup", UsernameChanged);
+
+    document.getElementById("createPassword").addEventListener("keyup", CreatePasswordChanged);
+    document.getElementById("confirmPassword").addEventListener("keyup", ConfrimPasswordChanged);
+
+}
+
+function CreatePasswordChanged() {
+    var password = document.getElementById("createPassword");
+    var regexExpress = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+
+    if (password.value.length === 0) {
+        whiteClass(password);
+    }
+    else if (password.value.search(regexExpress) === -1) {
+        redClass(password);
+    }
+    else {
+        greenClass(password);
+    }
+}
+
+function ConfrimPasswordChanged()
 {
-    $("#user").on('change', function () {
-        if($("#user").val().length < 5)
-        {
-            $("#user").addClass("errorBackground");
-        }
-        else
-        {
-            $("#user").removeClass("errorBackground");
-            $("#user").addClass("goodBackground");
-        }
-    });
+    var confirmPass = document.getElementById("confirmPassword");
+    var password = document.getElementById("createPassword");
 
-    //(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)
-    var password = $("#CreatePassword").val();
-    var confirmPass = $("#CreatePassword").val();
+    if (confirmPass.value.length === 0) {
+        whiteClass(confirmPass);
+    }
+    else if (confirmPass.value !== password.value) {
+        redClass(confirmPass);
+    }
+    else {
+        greenClass(confirmPass);
+    }
+}
+
+function UsernameChanged()
+{
+    var user = document.getElementById("user");
+    if (user.value.length === 0)
+    {
+       whiteClass(user);    
+    }
+    else if (user.value.length < 5) {
+        redClass(user);
+    }
+    else {
+        greenClass(user);
+    }
+}
+
+function redClass(el){
+    $(el).removeClass("whiteBackground");
+    $(el).removeClass("greenBackground");
+    $(el).addClass("redBackground");
+}
+
+function whiteClass(el) {
+    $(el).removeClass("redBackground");
+    $(el).removeClass("greenBackground");
+    $(el).addClass("whiteBackground");
+}
+
+function greenClass(el) {
+    $(el).removeClass("redBackground");
+    $(el).removeClass("whiteBackground");
+    $(el).addClass("greenBackground");
 }
 
