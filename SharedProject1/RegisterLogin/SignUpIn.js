0eb88c8a2d8ec1da93ec32629627e51d9481a696
oconnj01 <oconnj01@uwosh.edu>
2016-11-16 12:05:58 -0600

    did nothing

--- SharedProject1/RegisterLogin/SignUpIn.js
+++ SharedProject1/RegisterLogin/SignUpIn.js
@@ -3,18 +3,7 @@ window.onload = startup;
 
 function startUp()
 {
-    $("#user").on('change', function () {
-        if($("#user").val().length < 5)
-        {
-            $("#user").addClass("errorBackground");
-        }
-        else
-        {
-            $("#user").removeClass("errorBackground");
-            $("#user").addClass("goodBackground");
-        }
-    });
-
+    
     //(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)
     var password = $("#CreatePassword").val();
     var confirmPass = $("#CreatePassword").val();
