68ee89f4dd82a804364a0dea69e24e2a6a6b7ce8
oconnj01 <oconnj01@uwosh.edu>
2016-11-19 01:17:39 -0600

    Trying to upload phtotos

--- SharedProject1/Profile/upload.php
+++ SharedProject1/Profile/upload.php
@@ -1,44 +1,41 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-// Check if file already exists - change file name for user
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size - 5MB = 5 million bytes
-if ($_FILES["fileToUpload"]["size"] > 5000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
+	if(isset($_FILES["file"]["type"]))
+	{
+		$validextensions = array("jpeg", "jpg", "png");
+		$temporary = explode(".", $_FILES["file"]["name"]);
+		$file_extension = end($temporary);
+		
+		if ((($_FILES["file"]["type"] == "image/png") || 
+				($_FILES["file"]["type"] == "image/jpg") || 
+				($_FILES["file"]["type"] == "image/jpeg")) && 
+				($_FILES["file"]["size"] < 100000)//Approx. 100kb files can be uploaded.
+		&& in_array($file_extension, $validextensions)) 
+		{
+			if ($_FILES["file"]["error"] > 0)
+			{
+				echo "Return Code: " . $_FILES["file"]["error"] . "<br/><br/>";
+			}
+			else
+			{
+				if (file_exists("uploads/" . $_FILES["file"]["name"])) {
+					echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span> ";
+				}
+				else
+				{
+					$sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
+					$targetPath = "uploads/".$_FILES['file']['name']; // Target path where file is to be stored
+					move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
+					echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
+					echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
+					echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
+					echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+					echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
+				}
+			}
+		}
+		else
+		{
+			echo "<span id='invalid'>***Invalid file Size or Type***<span>";
+		}
+	}
 ?>
