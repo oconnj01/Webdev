c0c4bb9e36f3196d9c82bd7ef443a25a8ca87a27
thielj46 <thielj46@uwosh.edu>
2016-11-30 11:20:50 -0600

    Fixed more links

--- SharedProject1/RegisterLogin/verifySignIn.php
+++ SharedProject1/RegisterLogin/verifySignIn.php
@@ -9,12 +9,17 @@
     <title>Webdev Project</title>
     <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="JavaScript/SignUpIn.js" type="text/javascript"></script>
+    <script src="../JavaScript/jquery-3.1.1.min.js"></script>
+    <script src="../JavaScript/SignUpIn.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="SignUp.css">
+	<!--
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="SignUp.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="JavaScript/SignUpIn.js" type="text/javascript"></script>
+    <script src="JavaScript/SignUpIn.js" type="text/javascript"></script>-->
 </head>
 <body>
 	<?php
