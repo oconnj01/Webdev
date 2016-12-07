167ae7d3c26525b868dc03ce443a318a58a7e861
Manuel <Manuel@CHEFCHINO>
2016-12-07 12:11:19 -0600

    idk what I changed but it wont let me pull until i commit

--- SharedProject1/RegisterLogin/login_session.php
+++ SharedProject1/RegisterLogin/login_session.php
@@ -10,20 +10,21 @@
     <?php
 	    $db = new PDO("mysql:dbname=salina99;host=localhost", "salina99", "Oshdatabase11");
 
-	    session_start();// Starting Session
+		
+			session_start();// Starting Session
 
-	    // Storing Session
-	    $email=$_SESSION["email"];
+			// Storing Session
+			$email=$_SESSION["email"];
 
-	    // SQL Query To Fetch Needed Information Of User
-	    $usernames = $db->query("SELECT Username FROM WebUser WHERE Email=$email");
+			// SQL Query To Fetch Needed Information Of User
+			$usernames = $db->query("SELECT Username FROM WebUser WHERE Email=$email");
 	
-	    $user = "";
-	    foreach($usernames as $username){
-		    $user = $username["Username"];
-	    }
+			$user = "";
+			foreach($usernames as $username){
+				$user = $username["Username"];
+			}
       
-      $_SESSION["username"] = $user;   
+			$_SESSION["username"] = $user; 
     ?>
   </body>
 </html>
